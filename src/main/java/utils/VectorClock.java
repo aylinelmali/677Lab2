5e8bd358a12f2eaa2c07d969fea7b7c6e892c303
Andreas Gerasimow <agerasimow@umass.edu>
2024-11-03 11:23:52 -0500

    Added vector clock functionality

--- src/main/java/utils/VectorClock.java
+++ src/main/java/utils/VectorClock.java
@@ -31,12 +31,24 @@ public class VectorClock {
 
     // Compare two vector clocks
     public static boolean isConcurrent(int[] vc1, int[] vc2) {
-        boolean greater = false, less = false;
+        return !isSmallerThan(vc1, vc2) && !isSmallerThan(vc2, vc1);
+    }
+
+    public static boolean isSmallerOrEqualThan(int[] vc1, int[] vc2) {
+        boolean smallerOrEqual = true;
+
         for (int i = 0; i < vc1.length; i++) {
-            if (vc1[i] > vc2[i]) greater = true;
-            if (vc1[i] < vc2[i]) less = true;
+            if (vc1[i] > vc2[i]) {
+                smallerOrEqual = false;
+                break;
+            }
         }
-        return greater && less; // Concurrent if neither is fully ahead of the other
+        return smallerOrEqual;
+    }
+
+    public static boolean isSmallerThan(int[] vc1, int[] vc2) {
+        if (Arrays.equals(vc1, vc2)) return false;
+        return isSmallerOrEqualThan(vc1, vc2);
     }
 }
 
