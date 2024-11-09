61550df4c54a6d4761af1233af3767166008f8ca
Aylin Elmali <elmaliaylin@gmail.com>
2024-11-09 09:16:03 -0500

    Add to override functions

--- src/main/java/peer/Buyer.java
+++ src/main/java/peer/Buyer.java
@@ -3,6 +3,7 @@ package peer;
 import product.Product;
 import utils.Logger;
 import utils.Messages;
+import utils.VectorClock;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
@@ -29,22 +30,37 @@ public class Buyer extends APeer{
 
     @Override
     public void discoverAck(Product product, boolean available, int[] traderTimestamp) throws RemoteException {
-
+        this.timestamp = VectorClock.merge(this.timestamp, traderTimestamp);
+        if(available){
+            initiateBuy(product, amount);
+        }
+        else{
+            buyNewProduct();
+            
+        }
     }
 
     @Override
     public void buyAck(Product product, boolean bought, int[] traderTimestamp) throws RemoteException {
-
+        this.timestamp = VectorClock.merge(this.timestamp, traderTimestamp);
+        if (bought){
+            buyNewProduct();
+        }
     }
 
     @Override
     public void offerAck(int[] sellerTimestamp) throws RemoteException {
-
+        // Do nothing. This peer is not a Seller
     }
 
     @Override
     public void pay(int price, int[] traderTimestamp) throws RemoteException {
+        // Do nothing. This peer is not a Seller
+    }
 
+    public void initiateBuy(Product product, int amount) throws RemoteException {
+        this.timestamp[peerID]++;
+        peers[coordinatorID].buy(product, amount, this.timestamp, peerID);
     }
 
     /**
