9bbda631d4cd3e0be02ae50e31b656b71b5b712c
Aylin Elmali <elmaliaylin@gmail.com>
2024-11-10 09:21:06 -0500

    Add imports to start function

--- src/main/java/AsterixAndTheTradingPost.java
+++ src/main/java/AsterixAndTheTradingPost.java
@@ -1,6 +1,20 @@
+import peer.Buyer;
+import peer.IPeer;
+import peer.Seller;
+import utils.Logger;
+import utils.TraderState;
+
+import java.net.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.*;
+
 public class AsterixAndTheTradingPost {
 
     public static void main(String[] args) {
 
+        int n = Integer.parseInt(args[0]);  // Number of peers
     }
 }
